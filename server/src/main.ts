5856449dbe366deb35e5b573c8c18b01c461439b
Fábio Barros Ribeiro <eduardogomesdiniz42@gmail.com>
2022-03-29 17:21:46 -0300

    graphql-voyager

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
   app.useGlobalPipes(new ValidationPipe());
 
   const PORT = process.env.PORT || 5000;
